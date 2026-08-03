227db1009fa1afd188d2be92fea55db6902904f1
Yeonje Park <yeonje1004@naver.com>
2026-08-03 09:59:45 +0900

    Day 2 practice update

--- Java_SpringBoot_Rest API /Java/login_server/app/src/main/java/login_server/App.java
+++ Java_SpringBoot_Rest API /Java/login_server/app/src/main/java/login_server/App.java
@@ -3,10 +3,11 @@ package login_server;
 
 public class App {
     public static void main(String[] args) {
+
         TokenProvider tp = new TokenProvider();
         AuthService service = new AuthService(tp);
 
-        // 회원 가입 프로세스를 가정한다.
+        // 회원 가입 프로세스를 가정한다..
         System.out.println("회원가입 진행...");
         service.register("alice", "pass1234");
 
@@ -14,5 +15,6 @@ public class App {
         System.out.println("로그인 진행...");
         String token = service.login("alice", "pass1234");
         System.out.println("발급된 JWT: " + token);
+
     }
 }
